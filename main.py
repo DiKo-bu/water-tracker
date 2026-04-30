e714ee6449ad9e157aeec691d78e7a6d74c5053d
DiKo-bu <kdmitri30@gmail.com>
2026-04-30 10:48:52 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 import flet as ft
 import flet_webview as fwv
 
+
 def main(page: ft.Page):
     page.padding = 0
     page.spacing = 0
 
+    def on_navigation(e):
+        e.prevent_default = False
+        page.update()
+
     webview = fwv.WebView(
         url="https://wells-based-autos-alternate.trycloudflare.com",
         expand=True,
+        javascript_enabled=True,
+        on_navigation_request=on_navigation,
     )
 
     page.add(
         ft.Column(
             controls=[
-                ft.Container(height=40),  # отступ под статус-бар, подбери 30-50
+                ft.Container(height=40),
                 webview,
             ],
             spacing=0,
@@ -21,5 +28,6 @@ def main(page: ft.Page):
         )
     )
     page.update()
+
 
 ft.app(target=main)
