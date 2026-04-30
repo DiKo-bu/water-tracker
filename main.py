cf0cf48ea6949f013e4f59f1e7ae1799fa7a0da7
DiKo-bu <kdmitri30@gmail.com>
2026-04-30 12:21:56 +0500

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def main(page: ft.Page):
         page.update()
 
     webview = fwv.WebView(
-        url="https://wells-based-autos-alternate.trycloudflare.com",
+        url="https://donald-open-bookmark-roulette.trycloudflare.com",
         expand=True,
         javascript_enabled=True,
     )
